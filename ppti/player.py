e513aa564ed001de74d030ca42dfe60176e3e551
Francisco <andrewplanet@gmail.com>
2016-10-23 20:23:29 -0400

    Add life property and a function to handle it

--- ppti/player.py
+++ ppti/player.py
@@ -2,6 +2,7 @@ class Player(object):
 
 	def __init__(self):
 		self._option = None
+		self._life = 3
 
 	@property
 	def option(self):
@@ -10,3 +11,19 @@ class Player(object):
 	@option.setter
 	def option(self, value):
 		self._option = value
+
+	def reset_option(self):
+		self._option = None
+
+	@property
+	def life(self):
+		return self._life
+
+	@life.setter
+	def handle_life(self, item):
+		# var_1, var_2 = (param_1, param_2) <- tuple
+		operator, value = item
+		if operator == "+":
+			self._life += value
+		elif operator == "-":
+			self._life -= value
